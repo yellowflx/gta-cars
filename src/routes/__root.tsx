ddde203726d2cf6d7a1b3ec25285554a2735a595
Ilya Gavrilenko <ilya.753753@gmail.com>
2025-03-08 04:49:05 +0500

    feat: add vercel analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import {Outlet, createRootRoute} from '@tanstack/react-router';
+import { Analytics } from "@vercel/analytics/react"
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
+      <Analytics/>
     </>
   ),
 });
